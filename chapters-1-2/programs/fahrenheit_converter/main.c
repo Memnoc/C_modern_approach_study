0953d75fa8f3d0d96e8d3d6d9c0d6efdc6fbab94
Memnoc <smartdroidesign@gmail.com>
2025-02-28 11:33:35 +0000

    fahrenheit converter and first exercise

--- chapters-1-2/programs/fahrenheit_converter/main.c
+++ chapters-1-2/programs/fahrenheit_converter/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+#define FREEZING_POINT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
 int main(void) {
   printf("== Fahrenheit Converter ==\n");
+
+  float fahrenheit, celsius;
+  printf("Enter the Fahrenheit temperature: ");
+  scanf("%f", &fahrenheit);
+
+  celsius = (fahrenheit - FREEZING_POINT) * SCALE_FACTOR;
+  printf("Celsius equivalent: %.1f\n", celsius);
+
   return 0;
 }
